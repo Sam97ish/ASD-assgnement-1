d734252f76246e756d57980da83f1a7a34f47476
Hussam Aldeen <h-ssom1997@hotmail.com>
2020-09-23 03:55:41 +0200

    insert is done and is tested.

--- assignment 1/src/main/assignment1/impl/MyAVL4StringsImpl.java
+++ assignment 1/src/main/assignment1/impl/MyAVL4StringsImpl.java
@@ -6,6 +6,14 @@ import main.assignment1.MyList;
 
 public class MyAVL4StringsImpl implements MyAVL4Strings {
 	
+	private static final int ALLOWED_IMBALANCE = 1;
+	private AVLNode root;
+	
+	//Constructor
+	public MyAVL4StringsImpl(){
+		root = null;
+	}
+	
 	private static class AVLNode{
 		
 		String element; // the data.
@@ -26,14 +34,127 @@ public class MyAVL4StringsImpl implements MyAVL4Strings {
 
 	}
 	
-	private int height(AVLNode t) {// to handle when the node is null.
+	// to handle when the node is null.
+	private int height(AVLNode t) {
 		return t==null ? -1 : t.height;
 	}
 	
+	//left-left single rotation.
+	private AVLNode rotateWithLeftChild(AVLNode node) {
+		AVLNode leftChild = node.left;
+		node.left = leftChild.right;
+		leftChild.right = node;
+		
+		node.height = Math.max(height(node.left), height(node.right)) + 1;
+		leftChild.height = Math.max(height(leftChild.left), node.height) +1;
+		
+		return leftChild;
+	}
+	
+	//right-right single rotation
+	private AVLNode rotateWithRightChild(AVLNode node) {
+		AVLNode RightChild = node.right;
+		node.right = RightChild.left;
+		RightChild.left = node;
+		
+		node.height = Math.max(height(node.left), height(node.right)) + 1;
+		RightChild.height = Math.max(node.height, height(RightChild.right)) +1;
+		
+		return RightChild;
+	}
+	
+	//double rotation : left child.
+	private AVLNode doubleWithLeftChild(AVLNode node) {
+		
+		node.left = rotateWithRightChild(node.left);
+		return rotateWithLeftChild(node);
+	}
+	
+	//double rotation : right child.
+	private AVLNode doubleWithRightChild(AVLNode node) {
+		
+		node.right = rotateWithLeftChild(node.right);
+		return rotateWithRightChild(node);
+	}
+	
+	//balance the Avl tree after an insertion assuming node is either balanced or within one of being balanced.
+	private AVLNode balance(AVLNode node) {
+		
+		if(node == null) {
+			return node;
+		}
+		//handles the imbalance when it's in the left subtree.
+		if(height(node.left) - height(node.right) > ALLOWED_IMBALANCE) {
+			
+			//checks if we need an inside rotation or an outside rotation.
+			if(height(node.left.left) >= height(node.left.right)) {
+				node = rotateWithLeftChild(node);
+			}else {
+				node = doubleWithLeftChild(node);
+			}
+			
+		}else {//handles the imbalance when it's in the right subtree.
+		if(height(node.right) - height(node.left) > ALLOWED_IMBALANCE) {
+			
+			//checks which rotation is needed as before.
+			if(height(node.right.right) >= height(node.right.left)) {
+				node = rotateWithRightChild(node);
+			}else {
+				node = doubleWithRightChild(node);
+			}
+		}
+		
+		}
+		
+		node.height = Math.max(height(node.left), height(node.right)) + 1;
+		return node;
+		
+		
+	}
+	
+	//inserts elm into tree after finding a suitable place and balances the tree afterwards.
+	private AVLNode insert(String elm, AVLNode node) {
+		
+		if(node == null) {
+			return new AVLNode(elm,null,null);
+		}
+		
+		int compareResult = elm.compareTo(node.element);
+		
+		if(compareResult < 0) {
+			node.left = insert(elm,node.left);
+		}else if(compareResult > 0) {
+			node.right = insert(elm,node.right);
+		}else {
+			
+			; //duplicate do nothing.
+			
+		}
+		return balance(node);
+	}
+	
+	//prints out the whole tree using inorder traversal.
+	private void printInOrder(AVLNode root) {
+		if(root != null) {
+			printInOrder(root.left);
+			System.out.println(root.element);
+			printInOrder(root.right);
+		}
+	}
+	
+	//printing out the tree using the specified traversal.
+	public void printTree() {
+		printInOrder(root);
+	}
+	
     @Override
+    /*
+     * @role: inserts element into the AVL tree and rebalances if necessary.
+     * @complexity : O(logN). because the height of the Avl tree is LogN.
+     */
     public void insert(String element) {
 	// TODO Auto-generated method stub
-
+    	this.root = insert(element,root);
     }
 
     @Override
---
+++ assignment 1/src/main/assignment1/impl/main.java
@@ -0,0 +1,53 @@
+package main.assignment1.impl;
+
+public class main {
+
+	public static void main(String[] args) {
+		MyAVL4StringsImpl AVLtree = new MyAVL4StringsImpl();
+		
+		/*
+		System.out.println("this should perform left double rotation: expected inorder traversal : B C D E S");
+		AVLtree.insert("E"); // should be root
+		AVLtree.insert("S"); //should be right
+		AVLtree.insert("D"); //should be left
+		AVLtree.insert("B"); //should be left
+		AVLtree.insert("C"); //should be left
+		
+		AVLtree.printTree();
+		*/
+		
+		/*
+		System.out.println("this should perform left single rotation: expected inorder traversal : A B D E S");
+		AVLtree.insert("E"); // should be root
+		AVLtree.insert("S"); //should be right
+		AVLtree.insert("D"); //should be left
+		AVLtree.insert("B"); //should be left
+		AVLtree.insert("A"); //should be left
+		
+		AVLtree.printTree();
+		*/
+		
+		/*
+		System.out.println("this should perform right single rotation: expected inorder traversal : V W X Y Z");
+		AVLtree.insert("W"); // should be root
+		AVLtree.insert("V"); //should be left
+		AVLtree.insert("X"); //should be right
+		AVLtree.insert("Y"); //should be right
+		AVLtree.insert("Z"); //should be right
+		
+		AVLtree.printTree();
+		*/
+		
+		/*
+		System.out.println("this should perform right double rotation: expected inorder traversal : S T W X Y");
+		AVLtree.insert("T"); // should be root
+		AVLtree.insert("S"); //should be left
+		AVLtree.insert("W"); //should be right
+		AVLtree.insert("Y"); //should be right
+		AVLtree.insert("X"); //should be right
+		
+		AVLtree.printTree();
+		*/
+	}
+
+}
